eee92e061e79b922d921663732f55d366f44b2fa
Alex Ford <forda14@cardiff.ac.uk>
2019-01-18 16:46:09 +0000

    change

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -4,7 +4,7 @@
  // console.log(Name);
 //}
 
-// constructing the set variables.
+// constructing the set variables - THIS WILL NEED TO BE UPDATED.
 
 var api = "http://127.0.0.1:3000/"
 var books = "books/"
@@ -16,17 +16,74 @@ var apiSearchUsers = "http://127.0.0.1:3000/search?type=user&barcode=" //"http:/
 // JS for search function on Users tab (GET)
 
 
-const searchUsers = function(searchUsers) { 
-  let inputName = document.getElementById('searchusers').value; // grabbing the "searchusers" value
+const searchUsers = function() { 
+  let inputName = document.getElementById('search_users_button').value; // grabbing the "searchusers" value
   let url= apiSearchUsers + inputName // constructing the URL
   let httpReq = new XMLHttpRequest(); // Constructing the request
   httpReq.addEventListener("load", processResponse)
   httpReq.open("GET", url)
   httpReq.send();
-  console.log(searchUsers)
+  return searchUsers // Is this returning the JSON objects as a string? I don't 
 }
 
-//const createList = function(parentElement) {
+const processResponse = function() {
+  let response = JSON.parse(this.response); // is this correct? It keeps throwing up errors and since I have no idea what I'm doing....
+  let outputDiv = document.getElementById("userresulttext"); // hoping this will mean that results get painted to the page.
+  let newList = CreateTableFromJSON(outputDiv); // createList needs to be a new function that fills into your ID'd area "userresulttext"
+  response.forEach(function(records) {
+  let addListItem = (newList, records.name);
+  });
+}
+
+const searchButton_1 = document.getElementById("search_users_button");
+  searchButton_1.addEventListener('click', searchUsers())
+
+const CreateTableFromJSON = function() {
+    
+  // EXTRACT VALUE FOR HTML HEADER. 
+  // ('Book ID', 'Book Name', 'etc')
+  var col = [];
+  for (var i = 0; i < searchUsers.length; i++) {
+      for (var key in searchUsers[i]) {
+          if (col.indexOf(key) === -1) {
+              col.push(key);
+            }
+        }
+    }
+
+  // CREATE DYNAMIC TABLE.
+  var table = document.createElement("table");
+
+  // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+  var tr = table.insertRow(-1);                   // TABLE ROW.
+
+  for (var i = 0; i < col.length; i++) {
+      var th = document.createElement("th");      // TABLE HEADER.
+      th.innerHTML = col[i];
+      tr.appendChild(th);
+  }
+
+  // ADD JSON DATA TO THE TABLE AS ROWS.
+  for (var i = 0; i < searchUsers.length; i++) {
+
+      tr = table.insertRow(-1);
+
+      for (var j = 0; j < col.length; j++) {
+          var tabCell = tr.insertCell(-1);
+          tabCell.innerHTML = searchUsers[i][col[j]];
+      }
+  }
+
+  // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+  var divContainer = document.getElementById("userresulttext");
+  divContainer.innerHTML = "";
+  divContainer.appendChild(table);
+}
+
+
+
+  //const createList = function(parentElement) {
  // let newList = document.createElement("ol");
  // parentElement.appendChild(newList);
  // return console.log(newList);
@@ -37,21 +94,6 @@ const searchUsers = function(searchUsers) {
 //  newItem.appendChild(document.createTextNode(textContent));
 //  parentList.appendChild(newItem);
 
-const processResponse = function() {
-  let response = JSON.parse(this.response); // is this correct? It keeps throwing up errors and since I have no idea what I'm doing....
-  console.log(response);
-  let outputDiv = document.getElementById("userresulttext"); // hoping this will mean that results get painted to the page.
-  let newList = createList(outputDiv); // createList needs to be a new function that fills into your ID'd area "userresulttext"
-  response.forEach(function(records) {
-    addListItem(newList, records.name);
-  });
-}
-
-
-
-
-const searchButton_1 = document.getElementById("search_users_button");
-  searchButton_1.addEventListener('click', searchUsers())
 
 
 
