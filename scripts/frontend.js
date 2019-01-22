74a4fb823de30ce282af1f66a0f496a790ffe5d3
Alex Ford <forda14@cardiff.ac.uk>
2019-01-22 15:53:40 +0000

    tweaked a little more

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,20 +1,17 @@
-//let searchButtonUsers = document.getElementById("search_users_button");
-//submitButton.addEventListener("click", function() {
- // let nameSearch = document.getElementById("Name").nodeValue;
- // console.log(Name);
-//}
-
+// table CREATETABLEFROMJSON take from - https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm on the 20/12/18
 // constructing the set variables - THIS WILL NEED TO BE UPDATED.
 
 var api = "http://127.0.0.1:3000/"
 var books = "books/"
 var authors = "authors/"
+var authorbooks = "authors/1/books"
 var users = "users/"
 var apiSearchUsers = "http://127.0.0.1:3000/search?type=user&barcode=" //"http://127.0.0.1:3000/search?type=barcode="
 var apiSearchBooks = "http://127.0.0.1:3000/search?type=book&title="
+var lendList = ""
+
 
 // JS for Search function on books tab (GET)
-
 
 
 
@@ -29,8 +26,8 @@ const searchBooks = function() {
 
 const processResponse2 = function() {
   let response = JSON.parse(this.response); // is this correct? It keeps throwing up errors and since I have no idea what I'm doing....
-  let outputDiv = document.getElementById("bookresulttext"); // 
-  let newList = CreateTableFromJSON(outputDiv); // 
+  document.getElementById("bookresulttext"); // 
+  CreateTableFromJSON2(response); // 
   response.forEach(function(records) {
   let addListItem = (newList, records.name);
   });
@@ -54,10 +51,8 @@ const searchUsers = function() {
 
 const processResponse = function() {
   let response = JSON.parse(this.response); // is this correct? It keeps throwing up errors and since I have no idea what I'm doing....
-  let outputDiv = document.getElementById("userresulttext"); // hoping this will mean that results get painted to the page.
-  let newList = CreateTableFromJSON(response); // createList needs to be a new function that fills into your ID'd area "userresulttext"
+  CreateTableFromJSON(response); // createList needs to be a new function that fills into your ID'd area "userresulttext"
   response.forEach(function(records) {
-  let addListItem = (newList, records.name);
   //return processResponse;
   });
 }
@@ -65,8 +60,8 @@ const processResponse = function() {
 const searchButton_1 = document.getElementById("search_users_button");
   searchButton_1.addEventListener('click', searchUsers)
 
-// add users, no worky.
 
+  // add users, no worky.
 
 debugger;
 const addUsers = function() { 
@@ -74,9 +69,8 @@ const addUsers = function() {
   let staffOrStudent = document.getElementById("staffcb").value; 
   let url = api + users 
   let barcode = Math.floor((Math.random() * 1000000) + 1)
-  let JSONUsers =  inputName + staffOrStudent
   let httpReq = new XMLHttpRequest();
-  httpReq.open("POST", url)
+  httpReq.open("POST", url);
   httpReq.setRequestHeader
   httpReq.send(JSON.stringify({"name": inputName , "barcode": barcode , "memberType": staffOrStudent}));
 }
@@ -85,9 +79,26 @@ const addUsers = function() {
 const add_users_button = document.getElementById("add_users_button");
  add_users_button.addEventListener('click', addUsers)
 
+// Book adding. 
 
+const addBooks = function() {
+  let inputName = document.getElementById("booktitle").value;
+  let inputName2 = document.getElementById("ISBN").value;
+  let inputName3 = document.getElementById("authoradd").value;
+  let url = api + authorbooks
+  let url1 = api + authors + inputName3;
+  let httpReq = new XMLHttpRequest();
+  httpReq.open("POST", url1);
+  httpReq.setRequestHeader;
+  httpReq.send(JSON.stringify({"Author": inputName3}))
+  httpReq.open("POST", url);
+  httpReq.setRequestHeader;
+  httpReq.send(JSON.stringify({"bookTitle": inputName, "bookISBN": inputName2 }))
+}
 
- 
+const add_books_button = document.getElementById("add_books_button");
+  add_books_button.addEventListener('click', addBooks)
+
 // probaly easier to just nail this out completely. 
 
 const CreateTableFromJSON = function(outputDiv) {
@@ -95,7 +106,7 @@ const CreateTableFromJSON = function(outputDiv) {
   // EXTRACT VALUE FOR HTML HEADER. 
   // ('Book ID', 'Book Name', 'etc')
   var col = [];
-  for (var i = 0; i < outputDiv.length; i++) {
+  for (var i = 0; i < 4; i++) {
       for (var key in outputDiv[i]) {
           if (col.indexOf(key) === -1) {
               col.push(key);
@@ -110,7 +121,7 @@ const CreateTableFromJSON = function(outputDiv) {
 
   var tr = table.insertRow(-1);                   // TABLE ROW.
 
-  for (var i = 0; i < col.length; i++) {
+  for (var i = 0; i < 4; i++) {
       var th = document.createElement("th");      // TABLE HEADER.
       th.innerHTML = col[i];
       tr.appendChild(th);
@@ -121,7 +132,7 @@ const CreateTableFromJSON = function(outputDiv) {
 
       tr = table.insertRow(-1);
 
-      for (var j = 0; j < col.length; j++) {
+      for (var j = 0; j < 4; j++) {
           var tabCell = tr.insertCell(-1);
           tabCell.innerHTML = outputDiv[i][col[j]];
       }
@@ -129,10 +140,53 @@ const CreateTableFromJSON = function(outputDiv) {
 
   // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
   var divContainer = document.getElementById("userresulttext");
-  divContainer.innerHTML = "";
-  divContainer.appendChild(table);
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+    divContainer.getElementById("userresulttext").style.paddingLeft = "20%";
 }
 
+const CreateTableFromJSON2 = function(outputDiv) {
+    
+  // EXTRACT VALUE FOR HTML HEADER. 
+  // ('Book ID', 'Book Name', 'etc')
+  var col = [];
+  for (var i = 0; i < 3; i++) {
+      for (var key in outputDiv[i]) {
+          if (col.indexOf(key) === -1) {
+              col.push(key);
+            }
+        }
+    }
+
+  // CREATE DYNAMIC TABLE.
+  var table = document.createElement("table");
+
+  // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+  var tr = table.insertRow(-1);                   // TABLE ROW.
+
+  for (var i = 0; i < 3; i++) {
+      var th = document.createElement("th");      // TABLE HEADER.
+      th.innerHTML = col[i];
+      tr.appendChild(th);
+  }
+
+  // ADD JSON DATA TO THE TABLE AS ROWS.
+  for (var i = 0; i < outputDiv.length; i++) {
+
+      tr = table.insertRow(-1);
+
+      for (var j = 0; j < 3; j++) {
+          var tabCell = tr.insertCell(-1);
+          tabCell.innerHTML = outputDiv[i][col[j]];
+      }
+  }
+
+  // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+  var divContainer = document.getElementById("bookresulttext");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
 
 
   //const createList = function(parentElement) {
